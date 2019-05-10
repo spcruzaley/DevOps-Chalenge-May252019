5d3adfcea9022cf993572cfb2112688f9c92dc93
Salvador.Perez <salvador.perez@digitalonus.com>
2019-05-10 14:48:04 -0500

    Shell improvements

--- start.sh
+++ start.sh
@@ -18,8 +18,20 @@ echo -n $'\E[00m'
 
 function print {
   echo
-  echo -n $'\E[36m' $1
+  echo -n $'\E[36mINFO: '$1
   echo $'\E[00m'
+}
+
+function loading {
+  spin='-\|/'
+
+  i=0
+  while [ $1 == 0 ]
+  do
+    i=$(( (i+1) %4 ))
+    printf "\E[33m\r$2 ${spin:$i:1}"
+    sleep .1
+  done
 }
 
 function clean {
@@ -31,18 +43,20 @@ function clean {
 
 function kubernetesConfig {
   print "Starting Kubernetes config"
-  echo
   # Set DB data
-  kubectl apply -f ./config/configmap.yml
+  kubectl apply -f ./config/configmap.yml > log 2>/dev/null
 
   # Create the persistence storage
-  kubectl apply -f ./config/storage.yml
+  kubectl apply -f ./config/storage.yml >> log 2>/dev/null
 
   # Create postgres deploy and service
-  kubectl create -f ./config/deployment.yml
+  kubectl create -f ./config/deployment.yml >> log 2>/dev/null
 
   # Create postgres service
-  kubectl create -f ./config/service.yml
+  kubectl create -f ./config/service.yml >> log 2>/dev/null
+
+  #Copy scripts needed (for DB & email)
+  copyScripts
 }
 
 function copyScripts {
@@ -52,7 +66,6 @@ function copyScripts {
   fi
 
   #Copy needed files to execute SQL querys
-  print "Copying files..."
   chmod +x ./db/script-db.sh
   cp ./db/create.sql /tmp/scripts/
   cp ./db/script-db.sh /tmp/scripts/
@@ -66,6 +79,48 @@ function copyScripts {
   cp ./email/token.pickle /tmp/scripts/
 }
 
+function podStatus {
+  # IMPORTANT NOTE: The app name should be the same as the deployment.yml
+  # file has on app tag
+  pod=`kubectl get pods -l app=postgres -o custom-columns=:metadata.name`
+  # Remove return
+  pod=$(echo $pod|tr -d '\n')
+  print "pod/${pod} created"
+
+  lastStatus=''
+  spin='-\|/'
+  ready='---'
+  statusForRunning=''
+  statusForError=''
+
+  print "This task could be take some time."
+  echo 
+  i=0
+  while [[ ( ! $statusForError =~ "rror" && -z $statusForRunning) ]]
+  do
+    i=$(( (i+1) %4 ))
+    printf "\r$ready - Status: $phaseStatus $statusForError $statusForRunning ${spin:$i:1}"
+    sleep .1
+    statusForError=`kubectl get pod $pod -o jsonpath="{.status.containerStatuses[*].state.waiting.reason}"`
+    statusForRunning=`kubectl get pod $pod -o jsonpath="{.status.containerStatuses[*].state.running.startedAt}"`
+    ready=`kubectl get pod $pod -o jsonpath="\E[33m[Ready: {.status.containerStatuses[*].ready}]"`
+    phaseStatus=`kubectl get pod $pod -o jsonpath="{.status.phase}"`
+  done
+  if [[ $statusForError =~ "rror" ]] ;  then
+    reason=`kubectl get pod $pod -o jsonpath="Status: {.status.containerStatuses[*].state.waiting.reason}"`
+    printf "\n\E[41m\n"
+    printf "Something wrong happened\n"
+    printf "Status: $statusForError\n"
+    printf "Reason: $reason\E[00m\n"
+    sh ./stop.sh
+    exit 0
+  fi
+  echo
+  startedAt=`kubectl get pod $pod -o jsonpath="{.status.containerStatuses[*].state.running.startedAt}"`
+  printf "\r$ready - $phaseStatus started at: $startedAt\E[00m\n"
+  print "Pod Running successfully !"
+}
+
 banner
 
 # In case that /tmp/data directory exist, delete it
@@ -73,9 +128,4 @@ clean
 
 # Kubernetes config
 kubernetesConfig
-
-# Copy neccesary scripts
-copyScripts
-
-print "Done !!!"
-echo
+podStatus
--- stop.sh
+++ stop.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 
-function print {
+function logger {
   echo
-  echo -n $'\E[36m' $1
-  echo $'\E[00m'
+  echo $'\E[36mINFO: '$1
+  echo $'\E[00m\n'
 }
 
-print "Shutting down Kubernetes services..."
-echo
+# IMPORTANT NOTE: The app name should be the same as the deployment.yml
+# file has on app tag
+pod=`kubectl get pods -l app=postgres -o custom-columns=:metadata.name`
+# Remove return
+pod=$(echo $pod|tr -d '\n')
 
-kubectl delete service postgres
-kubectl delete deployment postgres
-kubectl delete configmap postgres-config
-kubectl delete persistentvolumeclaim postgres-pv-claim
-kubectl delete persistentvolume postgres-pv-volume
+if [ -z $pod ] ; then
+  logger "POD don't exist"
+  exit 0
+fi
+
+logger "Shutting down Kubernetes services..."
+
+kubectl delete service postgres >> log 2>/dev/null
+kubectl delete deployment postgres >> log 2>/dev/null
+kubectl delete configmap postgres-config >> log 2>/dev/null
+kubectl delete persistentvolumeclaim postgres-pv-claim >> log 2>/dev/null
+kubectl delete persistentvolume postgres-pv-volume >> log 2>/dev/null
+
+lastStatus='0'
+tag='Terminating'
+status=''
+spin='-\|/'
+
+i=0
+while [ $lastStatus != 1 ]
+do
+  i=$(( (i+1) %4 ))
+  printf "\E[33m\r$tag ${spin:$i:1}"
+  sleep .1
+  kubectl get pod $pod -o jsonpath="{.status.containerStatuses[*].state.waiting.reason}" >> log 2>/dev/null
+  lastStatus=$?
+done
+
+echo
+logger "Shutdown completed !"
